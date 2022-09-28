7109cc683cb62140fbd7d8cdb04a1b498260f30e
Dylan Luo <dragonfly.luo@gmail.com>
2022-09-28 10:42:15 -0700

    Finished first version of the random pattern recovery program

--- randomPattern.py
+++ randomPattern.py
@@ -49,7 +49,7 @@ print("Transmission Matrix for the first combined speckle pattern matrix \n", TM
 print("Shape of the first Transmission matrix", TM_New.shape)
 
 
-R_Recover_Test_1_URL = "C:/Users/Dylan Luo/Documents/MMF Data (Updated with Recovered Images) - Dylan Luo/MMF data/randomPattern100/000001.tif"
+R_Recover_Test_1_URL = "C:/Users/Dylan Luo/Documents/MMF Data (Updated with Recovered Images) - Dylan Luo/MMF data/randomPattern100/000003.tif"
 R_Recover_Test_1_URL_Absolute_Path = os.path.abspath(R_Recover_Test_1_URL)
 R_Recover_Test_1 = cv2.imread(R_Recover_Test_1_URL_Absolute_Path, cv2.IMREAD_UNCHANGED)
 R_Recover_Test_1 = R_Recover_Test_1.astype("float64")
@@ -58,6 +58,15 @@ R_Recover_Test_1_Vector = np.reshape(R_Recover_Test_1_Resize, (10000, 1))
 np.seterr(invalid="ignore")
 H_Recover_Test_1 = np.matmul(np.linalg.pinv(TM_New), R_Recover_Test_1_Vector)
 H_Recover_Test_1_Reshape = np.reshape(H_Recover_Test_1, (16, 16))
+
+H_Recover_Test_1_Sum = 0
+H_Recover_Test_1_Count = 0
+for i in range(len(H_Recover_Test_1_Reshape)):
+    for j in range(len(H_Recover_Test_1_Reshape[i])):
+        H_Recover_Test_1_Sum += float(H_Recover_Test_1_Reshape[i][j])
+        H_Recover_Test_1_Count += 1
+H_Recover_Test_1_Average = float(H_Recover_Test_1_Sum)/float(H_Recover_Test_1_Count)
+
 H_Recover_Test_1_Reshape_Threshed = H_Recover_Test_1_Reshape.copy()
 H_Recover_Test_1_Reshape_Threshed[H_Recover_Test_1_Reshape_Threshed >= -0.004] = 1
 H_Recover_Test_1_Reshape_Threshed[H_Recover_Test_1_Reshape_Threshed < -0.004] = 0
@@ -65,8 +74,9 @@ print("Test Image Recovery 1 without Thresh \n", H_Recover_Test_1_Reshape)
 print("Shape of Test Image Recovery without Thresh \n", H_Recover_Test_1_Reshape.shape)
 print("Test Image Recovery 1 with Thresh \n", H_Recover_Test_1_Reshape_Threshed)
 print("Shape of Test Image Recovery with Thresh \n", H_Recover_Test_1_Reshape_Threshed.shape)
+print("Average Value in the Test Image Recovery 1 \n", H_Recover_Test_1_Average)
 
-R_Recover_Test_2_URL = "C:/Users/Dylan Luo/Documents/MMF Data (Updated with Recovered Images) - Dylan Luo/MMF data/randomPattern100/000001.tif"
+R_Recover_Test_2_URL = "C:/Users/Dylan Luo/Documents/MMF Data (Updated with Recovered Images) - Dylan Luo/MMF data/randomPattern100/000004.tif"
 R_Recover_Test_2_URL_Absolute_Path = os.path.abspath(R_Recover_Test_2_URL)
 R_Recover_Test_2 = cv2.imread(R_Recover_Test_2_URL_Absolute_Path, cv2.IMREAD_UNCHANGED)
 R_Recover_Test_2 = R_Recover_Test_2.astype("float64")
@@ -75,28 +85,90 @@ R_Recover_Test_2_Vector = np.reshape(R_Recover_Test_2_Resize, (10000, 1))
 np.seterr(invalid="ignore")
 H_Recover_Test_2 = np.matmul(np.linalg.pinv(R_New_Update), R_Recover_Test_2_Vector)
 H_Recover_Test_2_Reshape = np.reshape(H_Recover_Test_2, (16, 16))
+
+H_Recover_Test_2_Sum = 0
+H_Recover_Test_2_Count = 0
+for i in range(len(H_Recover_Test_2_Reshape)):
+    for j in range(len(H_Recover_Test_2_Reshape[i])):
+        H_Recover_Test_2_Sum += float(H_Recover_Test_2_Reshape[i][j])
+        H_Recover_Test_2_Count += 1
+H_Recover_Test_2_Average = float(H_Recover_Test_2_Sum)/float(H_Recover_Test_2_Count)
+
 H_Recover_Test_2_Reshape_Threshed = H_Recover_Test_2_Reshape.copy()
-H_Recover_Test_2_Reshape_Threshed[H_Recover_Test_2_Reshape_Threshed >= -0.004] = 1
-H_Recover_Test_2_Reshape_Threshed[H_Recover_Test_2_Reshape_Threshed < -0.004] = 0
+H_Recover_Test_2_Reshape_Threshed[H_Recover_Test_2_Reshape_Threshed >= -0.001] = 1
+H_Recover_Test_2_Reshape_Threshed[H_Recover_Test_2_Reshape_Threshed < -0.001] = 0
 print("Test Image Recovery 2 without Thresh \n", H_Recover_Test_2_Reshape)
 print("Shape of Test Image Recovery 2 without Thresh \n", H_Recover_Test_2_Reshape.shape)
 print("Test Image Recovery 2 with Thresh \n", H_Recover_Test_2_Reshape_Threshed)
 print("Shape of Test Image Recovery 2 with Thresh \n", H_Recover_Test_2_Reshape_Threshed.shape)
+print("Average Value in the Test Image Recovery 2 \n", H_Recover_Test_2_Average)
 
 plt.subplot(121)
 plt.title("Recovered Test Image 1 \n without Thresh")
 plt.imshow(H_Recover_Test_1_Reshape)
 plt.subplot(122)
-plt.title("Recovered Test Image 2 \n without Thresh")
-plt.imshow(H_Recover_Test_2_Reshape)
+plt.title("Recovered Test Image 1\n with Thresh")
+plt.imshow(H_Recover_Test_1_Reshape_Threshed)
 plt.show()
 
 plt.subplot(121)
-plt.title("Recovered Test Image 1 \n with Thresh")
-plt.imshow(H_Recover_Test_1_Reshape_Threshed)
+plt.title("Recovered Test Image 2 \n without Thresh")
+plt.imshow(H_Recover_Test_2_Reshape)
 plt.subplot(122)
 plt.title("Recovered Test Image 2 \n with Thresh")
 plt.imshow(H_Recover_Test_2_Reshape_Threshed)
 plt.show()
+
+H_Recover_Average_List = []
+
+for w in range(100):
+    R_Recover_URL = "C:/Users/Dylan Luo/Documents/MMF Data (Updated with Recovered Images) - Dylan Luo/MMF data/randomPattern100/" + Random_Pattern_Data_List[w]
+    R_Recover_URL_Absolute_Path = os.path.abspath(R_Recover_URL)
+    R_Recover = cv2.imread(R_Recover_URL_Absolute_Path, cv2.IMREAD_UNCHANGED)
+    R_Recover = R_Recover.astype("float64")
+    R_Recover_Resize = cv2.resize(R_Recover, (100, 100), interpolation = cv2.INTER_LINEAR)
+    R_Recover_Vector = np.reshape(R_Recover_Resize, (10000, 1))
+    np.seterr(invalid="ignore")
+    H_Recover = np.matmul(np.linalg.pinv(TM_New), R_Recover_Vector)
+    H_Recover_Reshape = np.reshape(H_Recover, (16, 16))
+
+    H_Recover_Sum = 0
+    H_Recover_Count = 0
+    for u in range(len(H_Recover_Reshape)):
+        for v in range(len(H_Recover_Reshape[u])):
+            H_Recover_Sum += float(H_Recover_Reshape[u][v])
+            H_Recover_Count += 1
+    H_Recover_Average = float(H_Recover_Sum)/float(H_Recover_Count)
+    H_Recover_Average_List.append(H_Recover_Average)
+
+    H_Recover_Reshape_Threshed = H_Recover_Reshape.copy()
+    H_Recover_Reshape_Threshed[H_Recover_Reshape_Threshed >= -0.001] = 1
+    H_Recover_Reshape_Threshed[H_Recover_Reshape_Threshed < -0.001] = 0
+
+    Save_H_URL = "C:/Users/Dylan Luo/Documents/MMF Data (Updated with Recovered Images) - Dylan Luo/MMF data/Recovered Random Patterns/" + Random_Pattern_Data_List[w]
+    Save_H_URL_Absolute_Path = os.path.abspath(Save_H_URL)
+
+    plt.subplot(121)
+    plt.title("Recovered Random Pattern without Thresh")
+    plt.imshow(H_Recover_Reshape)
+    plt.subplot(122)
+    plt.title("Recovered Random Pattern with Thresh")
+    plt.imshow(H_Recover_Reshape_Threshed)
+    plt.savefig(Save_H_URL_Absolute_Path)
+
+
+H_Recover_Average_Overall_Sum = 0
+for a in range(len(H_Recover_Average_List)):
+    H_Recover_Average_Overall_Sum += float(H_Recover_Average_List[a])
+H_Recover_Average_Overall = float(H_Recover_Average_Overall_Sum)/float(len(H_Recover_Average_List))
+
+Save_Average_URL = "C:/Users/Dylan Luo/Documents/MMF Data (Updated with Recovered Images) - Dylan Luo/MMF data/Recovered Random Patterns/Random Patterns Average Values.txt"
+Save_Average_URL_Absolute_Path = os.path.abspath(Save_Average_URL)
+Average_Write = open(Save_Average_URL_Absolute_Path, "a")
+Average_Write.write(str(H_Recover_Average_List))
+Average_Write.write("\n \n" + str(H_Recover_Average_Overall))
+Average_Write.close()
+
+
 
 
